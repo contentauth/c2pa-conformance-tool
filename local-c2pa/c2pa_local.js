97b07e0541aae05b00878fe8ed9a92e98ad04a9c
Leonard Rosenthol <lrosenth@adobe.com>
2026-03-19 20:14:54 -0400

    Updates

--- local-c2pa/c2pa_local.js
+++ local-c2pa/c2pa_local.js
@@ -316,12 +316,12 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { dtor_idx: 183, function: Function { arguments: [Externref], shim_idx: 300, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { dtor_idx: 188, function: Function { arguments: [Externref], shim_idx: 299, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__h0667ea7aa0a255a7, wasm_bindgen__convert__closures_____invoke__hca39902318df4249);
             return ret;
         },
         __wbindgen_cast_0000000000000002: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { dtor_idx: 183, function: Function { arguments: [], shim_idx: 434, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { dtor_idx: 188, function: Function { arguments: [], shim_idx: 433, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__h0667ea7aa0a255a7, wasm_bindgen__convert__closures_____invoke__hdd2e4c5eb311bd94);
             return ret;
         },
